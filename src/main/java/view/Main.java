3b66808e452f118fadb42b4aa77d5f946c1b50fa
Almar <almar456@hotmail.com>
2020-01-29 14:18:12 +0100

    x

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 import database.TargetDatabase.TargetDatabaseDAOOracleImpl;
+import database.ToolDatabase.ToolDatabaseDaoOracleImpl;
 
 public class Main extends Application {
     private Stage primaryStage;
