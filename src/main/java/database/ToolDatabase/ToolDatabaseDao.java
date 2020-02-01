e9083710063b03e3bb43b6b6b2263a8ee0f5eea3
daanvandehaar <vandehaard@gmail.com>
2020-02-01 16:34:36 +0100

    TCMP

--- src/main/java/database/ToolDatabase/ToolDatabaseDao.java
+++ src/main/java/database/ToolDatabase/ToolDatabaseDao.java
@@ -7,14 +7,24 @@ import domain.definer.Rule;
 
 public interface ToolDatabaseDao {
 	public void updateRuleById(int id, String operator, String status);
+
 	public ArrayList<Attribute> getAttributesByRule(int ruleId);
+
 	public ArrayList<String> getValuesByAttribute(int attributeId);
+
 	public int addAttribute(int ruleId, String name, String entity);
+
 	public ArrayList<Rule> getAllRules();
+
 	public int addRule(String code, String SQLCode, int categoryId, String operator, String description, String status);
+
 	public void addValue(int attributeId, String value);
+
 	public ArrayList<Rule> getRulesByCode(String code);
+
 	public ArrayList<Rule> getRulesByEntity(String entity);
-}
+
 	public List<String> getIdsOfSetTriggers();
+
 	}
+
